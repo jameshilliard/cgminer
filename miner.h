166a837ba9b04d6a931f5cbdcdb2bfe122955f29
Zwilla <info@zwilla.de>
2016-03-16 20:41:30 +0100

    Update miner.h

--- miner.h
+++ miner.h
@@ -1,3 +1,15 @@
+/* Copyright 2016 Miguel Padilla
+ * Copyright 2011-2015 Con Kolivas
+ * Copyright 2011-2015 Andrew Smith
+ * Copyright 2011-2012 Luke Dashjr
+ * Copyright 2010 Jeff Garzik
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.  See COPYING for more details.
+ */
+ 
 #ifndef __MINER_H__
 #define __MINER_H__
 
